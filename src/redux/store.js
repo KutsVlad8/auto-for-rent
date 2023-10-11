cd44cddf7564cc24790f18725341e57d72a6fe02
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-10-11 16:47:34 +0300

    add persistr

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,33 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { carsReducer } from './carsSlice';
 
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import persistReducer from 'redux-persist/es/persistReducer';
+
+const carsPersistConfig = {
+  key: 'cars',
+  storage,
+};
+
 export const store = configureStore({
   reducer: {
-    cars: carsReducer,
+    cars: persistReducer(carsPersistConfig, carsReducer),
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
