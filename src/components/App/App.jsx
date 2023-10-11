a8965feb90f377bd7c2ae45a3d18332f00a08864
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-10-11 22:06:03 +0300

    update route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
           <Route index element={<Home />} />
           <Route path="/catalog" element={<Catalog />} />
           <Route path="/favorite" element={<Favorite />} />
+          <Route path="*" element={<Home />} />
         </Route>
       </Routes>
     </>
